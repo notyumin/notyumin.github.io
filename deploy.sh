e7744cd8e6a4fc954a220b832a0519ab0264e6c6
notyumin <notyumin@gmail.com>
2025-03-01 20:25:34 +0800

    Add .nojekyll to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]; then
   rm out/ -rf
-  npm run build   # build using next
-  git add out/ -f # force add public/ (cuz it's ignored)
+  npm run build       # build using next
+  touch out/.nojekyll # add .nojekyll so _next/ can be pushed
+  git add out/ -f     # force add public/ (cuz it's ignored)
   git commit -m "Commit to deploy"
   git push origin $(git subtree split --prefix out main):gh-pages --force
   git reset HEAD~
